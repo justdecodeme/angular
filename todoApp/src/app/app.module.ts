fa3dc09f19c0357a4c615ce77a0b7398ab82e5ae
Rakesh <justdecodeme@hotmail.com>
2018-09-04 22:02:52 +0530

    started working on firstapp

--- todoApp/src/app/app.module.ts
+++ todoApp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- todoApp/src/environments/environment.ts
+++ todoApp/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAHXshrjTtsn7przEBgCDqqn49drnTImYE",
+    authDomain: "todoapp-8973c.firebaseapp.com",
+    databaseURL: "https://todoapp-8973c.firebaseio.com",
+    projectId: "todoapp-8973c",
+    storageBucket: "todoapp-8973c.appspot.com",
+    messagingSenderId: "555906286308"
+  }
 };
 
 /*
