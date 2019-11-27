2953e32ff46588a58ff43ffb29436db9d9957619
Rakesh <justdecodeme@hotmail.com>
2019-11-27 23:15:24 +0530

    recipe-book project inialized

--- references/directives/src/app/app.component.ts
+++ references/directives/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name:string = "Rakko";
+
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
 }
--- references/directives/src/app/app.module.ts
+++ references/directives/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
@@ -12,7 +13,8 @@ import { BetterHighlightDirective } from './better-highlight/better-highlight.di
     BetterHighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
